7bf897952c0b0996700516862e1e0b4b45166617
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-07-19 12:17:54 -0300

    fazendo o botão de delte

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import { React, useState, useCallback } from "react";
-import { Container, Form, SubmitButton } from "./styles";
-import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
+import { Container, Form, SubmitButton, List, DeleteButton } from "./styles";
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from "react-icons/fa";
 import api from "../../services/api";
 
 export default function Main() {
@@ -43,6 +43,11 @@ export default function Main() {
     [newRepo, repositorios]
   ); //vai chamar a função apenas qunado o newRepo e o repositorios mudarem
 
+  const handleDelete = useCallback((repo)=>{
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  },[repositorios]);
+
   return (
     <div>
       <Container>
@@ -65,6 +70,22 @@ export default function Main() {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositorios.map((repo) => (
+            <li key={repo.name}>
+              <span>
+                <DeleteButton onClick={()=> handleDelete(repo.name)}>
+                  <FaTrash size={14} />
+                </DeleteButton>
+                {repo.name}
+              </span>
+              <a href="">
+                <FaBars size={20} />
+              </a>
+            </li>
+          ))}
+        </List>
       </Container>
     </div>
   );
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,38 +1,37 @@
-import styled,{keyframes, css} from "styled-components";
+import { type } from "@testing-library/user-event/dist/type";
+import styled, { keyframes, css } from "styled-components";
 
 export const Container = styled.div`
-max-width: 700px;
-background: #FFF;
-border-radius: 4px;
-box-shadow: 0 0 20px rgba(0,0,0,0.2);
-padding:30px;
-margin: 80px auto;
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
 
-h1{
+  h1 {
     font-size: 20px;
     display: flex;
     flex-direction: row;
     align-items: center;
-    
-    svg{
-        margin-right: 10px;
-    
-    }
-}
 
+    svg {
+      margin-right: 10px;
+    }
+  }
 `;
 export const Form = styled.form`
-    margin-top: 30px;
-    display: flex;
-    flex-direction:row;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
 
-    input{
-        flex: 1;
-        border: 1px solid #eee;
-        padding: 10px 15px;
-        border-radius: 4px;
-        font-size: 17px;
-    }
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 17px;
+  }
 `;
 
 //criando a animiação do botão
@@ -45,30 +44,62 @@ to{
 }
 `;
 
-export const SubmitButton = styled.button.attrs(props =>({
-    type:"submit",
-    disabled: props.loading,
+export const SubmitButton = styled.button.attrs((props) => ({
+  type: "submit",
+  disabled: props.loading,
 }))`
-    background: #0D2636;
-    border: 0;
-    border-radius: 4px;
-    margin-left: 10px;
-    padding: 0 15px;
+  background: #0d2636;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${animate} 2s linear infinite;
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 20px;
+
+  li {
+    padding: 15px 0;
     display: flex;
-    justify-content: center;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: center;
 
-
-    &[disabled]{
-        cursor: not-allowed;
-        opacity: 0.5;
+    & + li {
+      border-top: 1px solid #eee;
     }
 
-    ${
-        props => props.loading && css`
-        svg{
-            animation: ${animate} 2s linear infinite;
-        }
-        `
+    a {
+      color: #0d2636;
+      text-decoration: none;
     }
+  }
+`;
+
+export const DeleteButton = styled.button.attrs({
+  type: "button",
+})`
+  background: transparent;
+  color: #0d2636;
+  border: 0;
+  padding: 8px 7px;
+  border-radius: 4px;
+  outline: 0;
 `;
