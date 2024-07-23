6ded001bd359a5d147b971b8b543d97f06f60300
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-07-23 10:03:29 -0300

    criando o o header do repositorio

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,22 +1,22 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import api from "../../services/api";
-import { Container } from "./style";
-
+import { FaArrowLeft } from "react-icons/fa";
+import { Container, Owner, Loading, BackButton } from "./style";
 
 export default function Repositorio({ match }) {
   const [repositorio, setRepositorio] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
-  useEffect(()=>{
-    async function load(){
+  useEffect(() => {
+    async function load() {
       const nomeRepo = decodeURIComponent(match.params.repositorio);
       const [repositorioData, issuesData] = await Promise.all([
         api.get(`/repos/${nomeRepo}`),
-        api.get(`/repos/${nomeRepo}/issues`,{
-          params:{
-            state:'open',
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: "open",
             page: 5,
-          }
+          },
         }),
       ]);
 
@@ -26,6 +26,25 @@ export default function Repositorio({ match }) {
     }
 
     load();
-  }, [march.params.repositorio]);
-  return <Container></Container>;
+  }, [match.params.repositorio]);
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    );
+  }
+  return (
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+      <Owner>
+        <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login} />
+        <h1>{repositorio.name}</h1>
+        <h1>{repositorio.description}</h1>
+      </Owner>
+    </Container>
+  );
 }
